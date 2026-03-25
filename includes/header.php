511a5357c73181ab0c27bdfd296b56485d5dc543
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-25 13:23:56 +0530

    Added responsive Bootstrap navbar with brand and toggle button

--- includes/header.php
+++ includes/header.php
@@ -23,7 +23,11 @@
 
       <meta name="theme-color" content="#712cf9">
 
-
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">auth sys</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
 
 
 
