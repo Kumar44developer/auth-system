06b53649fd97cfb5371f306d6debbf095bda0e6a
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-25 13:24:47 +0530

    Added right-aligned navbar wit

--- includes/header.php
+++ includes/header.php
@@ -31,7 +31,10 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
 
 
 
