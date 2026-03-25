d5ef824cb505164cfafd6010599b6fd73ca74008
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-25 13:26:11 +0530

    Added dropdown menu with Logout option for authenticated users

--- includes/header.php
+++ includes/header.php
@@ -46,7 +46,9 @@
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             <?php echo $_SESSION['username']; ?>
-
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="logout.php">Logout</a></li>
 
 
 
