8c311a50deb2828d6c57803f4042823c175e749d
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-26 13:22:18 +0530

    Handled form input and query database to fetch user by email for login

--- login.php
+++ login.php
@@ -11,7 +11,12 @@
     if(isset($_POST['submit'])) {
       if($_POST['email'] == '' OR $_POST['password'] == '') {
         echo "some inputs are empty";
+      } else {
 
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $login = $conn->query("SELECT * FROM users WHERE email = '$email'");
 
 
 
